94c715fa23bb5c421b5f8d28034ca279de86aee8
Nerros1984 <javierhernandezav@gmail.com>
2025-05-15 22:08:29 +0200

    Update helpers.py
    Signed-off-by: Nerros1984 <javierhernandezav@gmail.com>

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,11 +1,14 @@
-# utils/helpers.py
 import locale
 from datetime import datetime
 
 def get_day_name_es(fecha_str):
     try:
-        locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
-    except locale.Error:
-        locale.setlocale(locale.LC_TIME, 'es_ES')  # fallback
+        locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')  # Linux/Mac
+    except:
+        try:
+            locale.setlocale(locale.LC_TIME, 'es_ES')  # Windows
+        except:
+            return "Día"
+
     fecha = datetime.strptime(fecha_str, "%Y-%m-%d")
     return fecha.strftime("%A").capitalize()
