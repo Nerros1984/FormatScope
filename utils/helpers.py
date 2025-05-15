d45140dc25e1fb543b7542026e67284effba8785
Nerros1984 <javierhernandezav@gmail.com>
2025-05-15 22:33:25 +0200

    Update helpers.py
    Signed-off-by: Nerros1984 <javierhernandezav@gmail.com>

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,22 @@
-import calendar
+import locale
 from datetime import datetime
 
-def get_day_name_es(fecha_iso):
-    dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-    if isinstance(fecha_iso, str):
-        fecha = datetime.fromisoformat(fecha_iso)
+def get_day_name_es(fecha_iso: str) -> str:
+    try:
+        # Establecer la localización en español (funciona en local si está instalada)
+        locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+    except locale.Error:
+        # En muchos entornos (como Streamlit Cloud) esto no funciona, así que se hace a mano
+        dias = {
+            'Monday': 'Lunes',
+            'Tuesday': 'Martes',
+            'Wednesday': 'Miércoles',
+            'Thursday': 'Jueves',
+            'Friday': 'Viernes',
+            'Saturday': 'Sábado',
+            'Sunday': 'Domingo'
+        }
+        nombre_en = datetime.strptime(fecha_iso, "%Y-%m-%d").strftime('%A')
+        return dias.get(nombre_en, nombre_en)
     else:
-        fecha = fecha_iso
-    return dias[fecha.weekday()]
+        return datetime.strptime(fecha_iso, "%Y-%m-%d").strftime('%A').capitalize()
