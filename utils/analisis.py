9c55909baf3aa811dbbcefbc272934911a17cd35
Nerros1984 <javierhernandezav@gmail.com>
2025-05-15 21:54:52 +0200

    Delete utils/analisis.py
    Signed-off-by: Nerros1984 <javierhernandezav@gmail.com>

--- utils/analisis.py
+++
@@ -1,8 +0,0 @@
-def evaluar_parrilla(df):
-    resumen = {}
-    resumen["Total de programas"] = len(df)
-    if 'genero' in df.columns:
-        resumen["Géneros más comunes"] = df['genero'].value_counts().head(3).to_dict()
-    if 'franja' in df.columns and 'programa' in df.columns:
-        resumen["Franjas vacías"] = list(df[df['programa'].isna()]["franja"].unique())
-    return resumen
