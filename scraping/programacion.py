1f7506f6dc8532c1eb85760c787ad5611e6f50b7
Nerros1984 <javierhernandezav@gmail.com>
2025-05-15 12:45:04 +0200

    Update programacion.py
    actualización
    
    
    Signed-off-by: Nerros1984 <javierhernandezav@gmail.com>

--- scraping/programacion.py
+++ scraping/programacion.py
@@ -1,9 +1,10 @@
 from scraping.rtv_teletexto import obtener_desde_rtv_teletexto
 from scraping.teletexto_com import obtener_desde_teletexto_com
+from scraping.movistarplus import obtener_desde_movistarplus
 from scraping.fallback import plantilla_vacia
 
 def obtener_parrilla_web(canal):
-    for extractor in [obtener_desde_rtv_teletexto, obtener_desde_teletexto_com]:
+    for extractor in [obtener_desde_rtv_teletexto, obtener_desde_teletexto_com, obtener_desde_movistarplus]:
         try:
             df = extractor(canal)
             if not df.empty:
